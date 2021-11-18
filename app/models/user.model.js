fc6ffd84e456e0cccbb48fe9656b2e560057a183
Abubakar <abubakarblackhole5@gmail.com>
2021-11-18 19:01:44 +0500

    live Credientails and email update

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -46,11 +46,11 @@ const UserSchema = new mongoose.Schema({
 
 UserSchema.methods.createPasswordResetToken = function () {
   const resetToken = crypto.randomBytes(32).toString("hex");
+
   this.passwordResetToken = crypto
     .createHash("sha256")
     .update(resetToken)
     .digest("hex");
-  this.passwordResetExpires = Date.now() + 10 * 60 * 100;
 
   return resetToken;
 };
