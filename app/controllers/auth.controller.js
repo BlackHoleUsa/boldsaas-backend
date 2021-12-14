28b44bf56ffd59f2993c78abc49df6ea64516b64
Abubakar <abubakarblackhole5@gmail.com>
2021-12-14 12:02:08 +0500

    live

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -16,14 +16,35 @@ exports.signup = async (req, res) => {
     password: bcrypt.hashSync(req.body.password, 8),
     is_Admin: req.body.is_Admin,
   });
+  // var token = jwt.sign({ id: user.id }, config.secret, {
+  //     expiresIn: 86400, // 24 hours
+  //   });
+
+  const token = jwt.sign({ user_id: user._id }, config.secret, {
+    expiresIn: "86400", //24h
+  });
+
+  // save user token
+  user.token = token;
 
   const save = await user.save((err, user) => {
     if (err) {
       res.status(500).json({ message: err });
       return;
     }
-    return res.status(200).json({ message: "User added Successfully." });
+    console.log("user =>", user);
+    return res.status(200).json({ message: "User added Successfully.", user });
   });
+  // console.log("save => ", save);
+  // res.send(save);
+
+  // var token = jwt.sign({ id: user.id }, config.secret, {
+  //     expiresIn: 86400, // 24 hours
+  //   });
+
+  // const find = await User.updateOne(
+  //   { email: req.body.email },
+  //   { $set: { token: token } }
 };
 
 exports.signin = async (req, res) => {
@@ -112,52 +133,50 @@ exports.forgotPassword = async (req, res) => {
 };
 
 exports.resetPassword = async (req, res) => {
-
   const hashedToken = crypto
     .createHash("sha256")
     .update(req.params.token)
     .digest("hex");
 
-   await User.findOne({
-    passwordResetToken: hashedToken 
+  await User.findOne({
+    passwordResetToken: hashedToken,
   })
-  .exec()
-  .then((product) => {
-    
-    const generateTime = product.passwordResetExpires;
-    const currentTime = parseInt(moment().unix());
-    const diff = currentTime - generateTime;
-    if (diff < 600) {
-      product.password = bcrypt.hashSync(req.body.password, 8);
-      product.passwordResetToken = undefined;
-      product.passwordResetExpires = undefined;
-      product
-      .save((err,result)=>{
-        if(err){
-          res.status(500).send('System error and user not saved due to some issue')
-          return
-        }
-        res.status(200).json({
-          message: "Password changed successfully!",
-          result
+    .exec()
+    .then((product) => {
+      const generateTime = product.passwordResetExpires;
+      const currentTime = parseInt(moment().unix());
+      const diff = currentTime - generateTime;
+      if (diff < 600) {
+        product.password = bcrypt.hashSync(req.body.password, 8);
+        product.passwordResetToken = undefined;
+        product.passwordResetExpires = undefined;
+        product.save((err, result) => {
+          if (err) {
+            res
+              .status(500)
+              .send("System error and user not saved due to some issue");
+            return;
+          }
+          res.status(200).json({
+            message: "Password changed successfully!",
+            result,
+          });
         });
-      });
-    }
-    else{
-      product.passwordResetToken = undefined;
-      product.passwordResetExpires = undefined;
-      product.save();
-      return res.status(404).json({
-        status: 404,
-        message: "Time Expire",
-      });
-    }
-    })
-      .catch(err=>{
+      } else {
+        product.passwordResetToken = undefined;
+        product.passwordResetExpires = undefined;
+        product.save();
         return res.status(404).json({
           status: 404,
-          message: "User not Found",
-          err
+          message: "Time Expire",
         });
-      })
+      }
+    })
+    .catch((err) => {
+      return res.status(404).json({
+        status: 404,
+        message: "User not Found",
+        err,
+      });
+    });
 };
