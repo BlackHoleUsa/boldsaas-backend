c47821b2814073691972271070eb6cb8f1b342d4
Abubakar <abubakarblackhole5@gmail.com>
2021-12-15 17:37:31 +0500

    histroy added

--- app/cronjob/token.generation.js
+++ app/cronjob/token.generation.js
@@ -9,7 +9,7 @@ async function tokenGeneration() {
       data: { access_token },
     } = await axios({
       // url: "https://api-m.paypal.com/v1/oauth2/token", // for live
-      url: "https://api.sandbox.paypal.com/v1/oauth2/token",   for testing
+      url: "https://api.sandbox.paypal.com/v1/oauth2/token", // for testing
       method: "post",
       headers: {
         Accept: "application/json",
